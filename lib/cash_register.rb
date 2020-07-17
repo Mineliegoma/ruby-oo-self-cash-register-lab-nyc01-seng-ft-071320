34cbb466f748b71754d17de35c3009d9ffc8b4c0
“Mineliegoma” <garlandegoma@gmail.com>
2020-07-17 12:28:05 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+class CashRegister
+    attr_accessor :total, :discount , :items
+    
+    def initialize(discount =nil)
+        @total= 0
+        @discount = discount
+        @items=[]
+        
+    end
+    
 
+    def add_item(title, price, quantity=1)
+        quantity.times do 
+            items<<title
+        @last_total = @total
+        end
+
+        self.total+= price * quantity
+        #@items<< title
+    end
+    def apply_discount
+        if discount 
+            @total -= @total* @discount/100
+            "After the discount, the total comes to $#{self.total}."
+        else
+        "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction #it should undo whatever we just send to total
+
+       @total = @last_total
+     end
+
+    
+
+
+
+    
+
+end 
